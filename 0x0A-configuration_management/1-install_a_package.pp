c474e4b86739af0f98ef3a815c2205b1f99851e7
ZidiEya <3805@holbertonschool.com>
2022-05-01 07:55:24 -0700

    install flask from pip3.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
-# Using Puppet, install puppet-lint.
-package { 'puppet-lint':
+# Using Puppet, install puppet-lint
+exec { 'apt-get update':
+  command   => '/usr/bin/apt-get update'
+}
+
+package {'puppet-lint':
   ensure   => '2.1.0',
   provider => 'gem',
 }
